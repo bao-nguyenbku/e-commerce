3d9f146001a18305f0f3018015eadaa84dc4fdf0
andrewquang512 <andrew.viquang@gmail.com>
2022-04-29 01:23:12 +0700

    add paypal to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
-import Courses from "./components/Courses/Courses";
-import NavBar from "./components/NavBar/NavBar";
-import Banner from "./components/Banner/Banner";
-import Login from "./components/Login/Login";
-import CourseDetail from "./components/CourseDetail/CourseDetail";
-import Cart from "./components/Cart/Cart";
-import CourseLearning from "./components/CourseLearning/CourseLearning";
+import Courses from './components/Courses/Courses';
+import NavBar from './components/NavBar/NavBar';
+import Banner from './components/Banner/Banner';
+import Login from './components/Login/Login';
+import CourseDetail from './components/CourseDetail/CourseDetail';
+import Cart from './components/Cart/Cart';
+import CourseLearning from './components/CourseLearning/CourseLearning';
 
-import "./App.scss";
-import store from "./store/index";
-import { getCourses } from "./store/actions/course";
-
-import Checkout from "./components/Cart/Checkout/Checkout";
+import './App.scss';
+import store from './store/index';
+import { getCourses } from './store/actions/course';
+import Checkout from './components/Cart/Checkout/Checkout';
 
 const App = () => {
   useEffect(() => {
@@ -22,30 +22,38 @@ const App = () => {
   }, []);
 
   return (
-    <Provider store={store}>
-      <div className="wrapper">
-        <NavBar />
-        <div className="body-container">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/course/:id" element={<CourseDetail />} />
-            <Route
-              path="/"
-              element={
-                <>
-                  <Banner />
-                  <Courses />
-                </>
-              }
-            />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/cart/checkout" element={<Checkout />} />
+    <PayPalScriptProvider
+      options={{
+        'client-id': 'test',
+        components: 'buttons',
+        currency: 'USD',
+      }}
+    >
+      <Provider store={store}>
+        <div className='wrapper'>
+          <NavBar />
+          <div className='body-container'>
+            <Routes>
+              <Route path='/login' element={<Login />} />
+              <Route path='/course/:id' element={<CourseDetail />} />
+              <Route
+                path='/'
+                element={
+                  <>
+                    <Banner />
+                    <Courses />
+                  </>
+                }
+              />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/cart/checkout' element={<Checkout />} />
 
-            <Route path="/learning" element={<CourseLearning />} />
-          </Routes>
+              <Route path='/learning' element={<CourseLearning />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Provider>
+      </Provider>
+    </PayPalScriptProvider>
   );
 };
 
--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -3,10 +3,14 @@ import styles from './Checkout.module.scss';
 import paypal from '../../../images/paypal-logo.png';
 
 import OrderList from './OrderList/OrderList';
+import ButtonWrapper from '../../Ultils/Paypal/ButtonWrapper';
 import { useSelector } from 'react-redux';
+
+const currency = 'USD';
+
 const Checkout = () => {
-  const total = useSelector(state => state.cart.total);
-  const items = useSelector(state => state.cart.items);
+  const total = useSelector((state) => state.cart.total);
+  const items = useSelector((state) => state.cart.items);
   return (
     <div className={styles['background']}>
       <div className={styles['container']}>
@@ -14,31 +18,34 @@ const Checkout = () => {
           <p>Checkout</p>
           <div className={styles['billing-address']}>
             <p className={styles['title']}>Address</p>
-            <select name="" id={styles["country-select"]}>
-              <option value="vietnam">Vietnam</option>
-              <option value="usa">USA</option>
+            <select name='' id={styles['country-select']}>
+              <option value='vietnam'>Vietnam</option>
+              <option value='usa'>USA</option>
             </select>
           </div>
           <div className={styles['payment-method']}>
             <p className={styles['title']}>Payment method</p>
             <div className={styles['method-container']}>
               <div className={styles['method-panel']}>
-                <input type="radio" id='paypal' />
-                <label htmlFor="paypal">
-                  <img src={paypal} alt="" />
+                <input type='radio' id='paypal-option' />
+                <label htmlFor='paypal-option'>
+                  <img src={paypal} alt='' />
                   <p>PayPal</p>
                 </label>
               </div>
               <div className={styles['content']}>
-                <p>In order to complete your transaction, we will transfer you over to PayPal's secure servers.</p>
+                <p>
+                  In order to complete your transaction, we will transfer you
+                  over to PayPal's secure servers.
+                </p>
               </div>
             </div>
           </div>
           <div className={styles['orders']}>
             <p className={styles['title']}>Order Summary</p>
             <div className={styles['order-list']}>
-              {items.map(item => {
-                return <OrderList key={item.id} item={item}/>
+              {items.map((item) => {
+                return <OrderList key={item.id} item={item} />;
               })}
             </div>
           </div>
@@ -51,13 +58,19 @@ const Checkout = () => {
               <span>Total:</span>
               <span>{total} đ</span>
             </div>
+            <div style={{ maxWidth: '750px', minHeight: '200px' }}>
+              <ButtonWrapper currency={currency} />
+            </div>
             <button className={styles['proceed']}>Proceed</button>
-            <p className={styles['terms-of-service']}>By completing your purchase you agree with <strong>Terms of service</strong>.</p>
+            <p className={styles['terms-of-service']}>
+              By completing your purchase you agree with{' '}
+              <strong>Terms of service</strong>.
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
